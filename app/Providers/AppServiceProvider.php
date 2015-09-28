9b06701bf2069302da84c6cbb1ceae098774e47d
Glyn Jones <centralhawk3@gmail.com>
2015-09-28 10:14:46 -0400

    Applied fixes from StyleCI

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,8 +23,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        if ($this->app->environment() == 'local')
-        {
+        if ($this->app->environment() == 'local') {
             $this->app->register('Laracasts\Generators\GeneratorsServiceProvider');
         }
     }
